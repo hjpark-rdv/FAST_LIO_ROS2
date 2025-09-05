8a313116320e05683da4177859e85e368ec0fba0
hjpark <hjpark@accupix.com>
2025-09-05 09:55:27 +0900

    fast_lio param저장(mid360.yaml)

--- src/preprocess.h
+++ src/preprocess.h
@@ -131,6 +131,15 @@ typedef struct {
   uint8_t tag;        /**< Livox point tag   */
   uint8_t line;       /**< Laser line id     */
 } LivoxPointXyzitl;
+
+typedef struct {
+  float x;            /**< X axis, Unit:m */
+  float y;            /**< Y axis, Unit:m */
+  float z;            /**< Z axis, Unit:m */
+  float intensity;    /**< Intensity   */
+  uint8_t ring;       /**< ring for gazebo */
+} LivoxPointXyzir;
+
 }
 POINT_CLOUD_REGISTER_POINT_STRUCT(livox_ros::LivoxPointXyzrtl,
     (float, x, x)
@@ -148,6 +157,14 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(livox_ros::LivoxPointXyzitl,
     (float, intensity, intensity)
     (uint8_t, tag, tag)
     (uint8_t, line, line)
+)
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(livox_ros::LivoxPointXyzir,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, intensity, intensity)
+    (uint8_t, ring, ring)
 )
 
 class Preprocess
